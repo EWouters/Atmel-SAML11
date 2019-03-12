b8e18092cd12dd6dfdb03501f912d14deaf9b78c
Dragoș Perju <dragosprju@gmail.com>
2019-03-12 16:16:35 +0100

    Checkpoint on Kalman output comparer

--- KalmanC++/Tools/comparer.py
+++ KalmanC++/Tools/comparer.py
@@ -1,13 +1,18 @@
 
 import sys
 
-def compare_cells(cell_1, cell_2):
+# TODO: do in floats
 
+def compare_cells(cell_1, cell_2):
 	intg_wrong_positions = []
 	flot_wrong_positions = []
 
-	(intg1, flot1) = cell_1.split(".")
-	(intg2, flot2) = cell_2.split(".")
+	if "." in cell_1:
+		(intg1, flot1) = cell_1.split(".")
+		(intg2, flot2) = cell_2.split(".")
+	else:
+		intg1 = cell_1
+		intg2 = cell_2
 
 	smaller_string_len = min(len(intg1), len(intg2))
 
@@ -15,13 +20,12 @@ def compare_cells(cell_1, cell_2):
 		if intg1[i] != intg2[i]:
 			intg_wrong_positions.append(i)
 
-	intg_wrong_positions = intg_wrong_positions.sort()
+	intg_wrong_positions.sort()
 	if len(intg1) != len(intg2):
 		bigger_string_len = max(len(intg1), len(intg2))
 		intg_wrong_positions.extend(range(smaller_string_len, bigger_string_len))
-
-	print(intg_wrong_positions)
-		
+	
+	return intg_wrong_positions
 
 def compare_lines(line_1, line_2):
 	line_1_split = line_1.split(",")
@@ -30,7 +34,13 @@ def compare_lines(line_1, line_2):
 	line_1_split = [cell.strip() for cell in line_1_split]
 	line_2_split = [cell.strip() for cell in line_2_split]
 
-	compare_cells(line_1_split[1], line_2_split[1])
+	assert(len(line_1_split) == len(line_2_split))
+	#assert(len(line_1_split) == 2)
+
+	for i in range(len(line_1_split)):
+		ret = compare_cells(line_1_split[i], line_2_split[i])
+	
+		#print(cell1, cell2, ret)
 
 	return 0
 
