ba1fcf79f9bceacf115c6d9de2568cf70d564672
Dragoș Perju <dragosprju@gmail.com>
2019-03-25 11:01:46 +0100

    General updates on experiments

--- PythonCSV/debug.py
+++ PythonCSV/debug.py
@@ -1,7 +1,28 @@
-
+from pydgilib_extra import LOGGER_OBJECT, LOGGER_CSV, LOGGER_PLOT
 import dgilib_threaded as dgi_t
+import os
+from atprogram.atprogram import *
 
+dgilib_config_dict = {
+    "loggers": [LOGGER_OBJECT, LOGGER_CSV, LOGGER_PLOT],
+    "plot_pins": [False, True, False, False],
+    "plot_pins_values": [False, True, False, False],
+    "plot_pins_method": "highlight",
+    "gpio_delay_time": 0.0015
+}
 
-data, processed_data = dgi_t.start()
-dgi_t.wait_dgilib()
-dgi_t.wait_sendrecv()
+config = {
+    "input_acc_file": os.path.join("input", "input_acc.csv"),
+    "input_gyro_file": os.path.join("input", "input_gyro.csv"),
+    "output_file": os.path.join("output", "output_arm_debug.csv")
+}
+
+atprogram("C:\\Users\\Dragos\\Dropbox\\RISE\\Git\\KalmanARM\\STDIO_Redirect_w_TrustZone", verbose=1)
+
+data, processed_data = dgi_t.measure(
+    duration=5, iterations=5, dgilib_config_dict=dgilib_config_dict,
+    input_acc_file=config["input_acc_file"], input_gyro_file=config["input_gyro_file"], output_file=config["output_file"], waitForPlot=True
+)
+
+dgi_t.show_plot_for_data(data)
+
