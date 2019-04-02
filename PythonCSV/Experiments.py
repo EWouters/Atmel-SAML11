f35860ff45b63ee2528394984ce2a721cae8b13e
Dragoș Perju <dragosprju@gmail.com>
2019-04-02 11:36:00 +0200

    General updates to experiments

--- PythonCSV/Experiments.py
+++ PythonCSV/Experiments.py
@@ -2,15 +2,15 @@ from experiment.loop import loop
 import sys
 import os
 
-initial = 1 # 1 = Don't start with baseline
+initial = 1  # 1 = Don't start with baseline
 
 iterations = 2887
 
 hash_sizes = [120]
-epsilon=[0.8, 0.7, 0.6, 0.5]
-mod_precision=[10000]
+epsilon = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+mod_precision = [10000]
 
-values = [(0,0,0)]
+values = [(0, 0, 0)]
 
 for i in range(len(hash_sizes)):
     for j in range(len(epsilon)):
@@ -27,19 +27,22 @@ if __name__ == "__main__":
     print("At index {0}...".format(to_do))
 
     if to_do == 0:
-        print("Baseline:")
+        pass
+        # print("Baseline:")
         #loop("baseline", iterations, attempt=1, output_dir="output")
     else:
-        (h,e,m) = values[to_do]
-        
+        (h, e, m) = values[to_do]
+
         try:
             print("Hash {0}, Epsilon {1}, Mod precision {2}:".format(h, e, m))
-            loop("hash", hash_size=h, epsilon=e, mod_precision=m, iterations=iterations, attempt=1, program=True, output_dir="output")
+            loop("hash", hash_size=h, epsilon=e, mod_precision=m, iterations=iterations,
+                 attempt=1, program=True, output_dir="C:\\Users\\Dragos\\MasThesis_output")
         except MemoryError:
+            print("Retrying at index {0}\n".format(to_do))
             to_do -= 1
-            print("Retrying at index {0}".format(to_do))
-        
-        if to_do+1 < len(values):            
-            os.execv(sys.executable, [sys.executable, __file__] + [str(to_do + 1)])
+
+        if to_do+1 < len(values):
+            os.execv(sys.executable, [sys.executable,
+                                      __file__] + [str(to_do + 1)])
         else:
             print("Ending at index {0}...".format(to_do))
